729fad699dbf624443717176a55a55ffb2f44927
hoxmark <bhoxmark@gmail.com>
2023-11-12 13:27:08 +0100

    handle chapters

--- touch-type/src/components/ExerciseSelector.js
+++ touch-type/src/components/ExerciseSelector.js
@@ -1,34 +1,38 @@
 // src/components/ExerciseSelector.js
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css';
-import exercisesData from '../assets/exercises.json';
+// import '../App.css';
+import chaptersData from '../assets/exercises.json'; // Assuming the new JSON is named exercises.json but structured with "chapters"
+import '../css/ExerciseSelector.css';
 
-function ExerciseSelector(welcome) {
-    const [exercises, setExercises] = useState([]);
+function ExerciseSelector({ welcome }) {
+    const [chapters, setChapters] = useState([]);
 
     useEffect(() => {
-        setExercises(exercisesData.exercises);
-        //fetch('/api/exercises/')
-        //    .then(response => response.json())
-        //    .then(data => setExercises(data));
+        setChapters(chaptersData.chapters);
     }, []);
 
     return (
         <div className="app">
             <div className="header">
                 <h1>Select an Exercise</h1>
-                <p>{welcome.welcome}</p>
+                <p>{welcome}</p> {/* Render the welcome text */}
             </div>
-            <div className="card-container">
-                {exercises.map(exercise => (
-                    <div className="exercise-card" key={exercise.exercise_id}>
-                        <h2>{exercise.name}</h2>
-                        <p>{exercise.description}</p>
-                        <Link to={`/typing-exercise/${exercise.exercise_id}`}>Start Exercise</Link>
+            {chapters.map(chapter => (
+                <div key={chapter.chapter_id} className="chapter">
+                    <h2 className="chapter-title">{chapter.name}</h2>
+                    <p className="chapter-description">{chapter.description}</p>
+                    <div className="card-container">
+                        {chapter.exercises.map(exercise => (
+                            <div className="exercise-card" key={exercise.exercise_id}>
+                                <h3>{exercise.name}</h3>
+                                <p>{exercise.description}</p>
+                                <Link to={`/typing-exercise/${exercise.exercise_id}`}>Start Exercise</Link>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
     );
 }
--- touch-type/src/components/TypingExercise.js
+++ touch-type/src/components/TypingExercise.js
@@ -10,8 +10,7 @@ import ExerciseFooter from "./ExerciseFooter";
 import ExerciseHeader from "./ExerciseHeader";
 import Header from './Header';
 
-import exercisesData from '../assets/exercises.json';
-
+import chaptersData from '../assets/exercises.json'; // Updated import to reflect new data structure
 
 function TypingExercise() {
     const [userInput, setUserInput] = useState('');
@@ -19,44 +18,37 @@ function TypingExercise() {
     const [exercise, setExercise] = useState(null);
     const { exercise_id } = useParams();  // Access route parameters
 
-    useEffect(() => {
-        // Check if the task is completed and stop the timer if it is.
-        if (isCompleted) {
-            stopTimer();
-        }
-    }, [isCompleted]);
+    // useEffect(() => {
+    //     // Check if the task is completed and stop the timer if it is.
+    //     if (isCompleted) {
+    //         stopTimer();
+    //     }
+    // }, [isCompleted]);
 
     const {
         startTime,
         elapsedTime,
         WPM,
         startTimer,
-        stopTimer,
+        // stopTimer,
         calculateWPM
     } = useWPM();  // Use the custom hook
 
-
     useEffect(() => {
-        // Use the Array find method to find the exercise with the correct id
-        const foundExercise = exercisesData.exercises.find(ex => ex.exercise_id === exercise_id);
-        // Set the found exercise to state
+        // Find the correct exercise by iterating over all chapters
+        let foundExercise = null;
+        for (let chapter of chaptersData.chapters) {
+            foundExercise = chapter.exercises.find(ex => ex.exercise_id === exercise_id);
+            if (foundExercise) break;
+        }
         setExercise(foundExercise);
     }, [exercise_id]);
 
-    
-    //useEffect(() => {
-    //    fetch(`/api/exercise/${exercise_id}/`) // Use the exercise_id parameter in the API request
-    //        .then(response => response.json())
-    //        .then(data => setExercise(data));
-    //}, [exercise_id]); // Add exercise_id as a dependency
-
     const handleRestartClick = () => {
-        setUserInput(''); // Reset the text input state
-        const foundExercise = exercisesData.exercises.find(ex => ex.exercise_id === exercise_id); // Find the exercise data again
-        setExercise(foundExercise); // Reset the exercise state with the found exercise
+        setUserInput('');
+        setIsCompleted(false);
+        startTimer();  // Restart the timer for a new attempt
     };
-
-
 
     return (
         <div className="app">
@@ -64,17 +56,14 @@ function TypingExercise() {
             <section className="exercise-section">
                 {exercise && (
                     <div className="exercise-container">
-
-
                         <ExerciseHeader
                             title={exercise.name}
                             elapsedTime={elapsedTime}
                             WPM={WPM}
                         />
-                        {/* Display the Description Here */}
                         <p className="exercise-description">{exercise.description}</p>
                         <ExerciseContent
-                            content={exercise.tasks.toLowerCase()} // Convert to lowercase, temporary while imported data is in uppercase
+                            content={exercise.tasks.toLowerCase()}
                             userInput={userInput}
                             isCompleted={isCompleted}
                             onInputChange={e => {
@@ -82,10 +71,7 @@ function TypingExercise() {
                                     startTimer();
                                 }
                                 let newValue = e.target.value;
-
-                                // Replace newline character with return symbol
                                 newValue = newValue.replace(/\n/g, '⏎');
-
                                 if (newValue === exercise.tasks) {
                                     setIsCompleted(true);
                                 } else {
